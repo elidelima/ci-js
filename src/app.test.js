322850eb4fc95c48ff32842185d9ac9dc0c4ae3c
Eli Lima <elimigueldelima@gmail.com>
2024-07-08 16:40:47 +0100

    ci: make status check pass again

--- src/app.test.js
+++ src/app.test.js
@@ -2,6 +2,6 @@ const { sum } = require("./app")
 
 describe('app test suite', () => {
     it('add 7 + 5 shoulbe be equal to 12', () => {
-        expect(sum(5, 7)).toBe(13);
+        expect(sum(5, 7)).toBe(12);
     })
 })
