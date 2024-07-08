e48e4585def70f2cf1fbc3cf9e4b54e8c11a2d66
Eli Lima <elimigueldelima@gmail.com>
2024-07-08 17:41:44 +0100

    feat: add test to new method

--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,11 @@
-const { sum } = require("./app")
+const { sum, sub } = require("./app")
 
 describe('app test suite', () => {
-    it('does nothing', () => {
-    })    
-    // it('add 7 + 5 shoulbe be equal to 12', () => {
-    //     expect(sum(5, 7)).toBe(12);
-    // })
+    it('add 7 + 5 shoulbe be equal to 12', () => {
+        expect(sum(5, 7)).toBe(12);
+    })
+
+    it('sub 7 - 5 shoulbe be equal to 2', () => {
+        expect(sub(7, 5)).toBe(2);
+    })
 })
