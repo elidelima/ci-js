429f5b9f7592166615c6855b9cd6c12b76a89cb1
Eli Lima <elimigueldelima@gmail.com>
2024-07-08 17:29:07 +0100

    ci: make sonar cloud check fail

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,9 @@
 const { sum } = require("./app")
 
 describe('app test suite', () => {
-    it('add 7 + 5 shoulbe be equal to 12', () => {
-        expect(sum(5, 7)).toBe(12);
-    })
+    it('does nothing', () => {
+    })    
+    // it('add 7 + 5 shoulbe be equal to 12', () => {
+    //     expect(sum(5, 7)).toBe(12);
+    // })
 })
